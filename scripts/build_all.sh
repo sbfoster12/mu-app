acb8cb09e9b288eb81e4cfc8779652bd022c8a64
Sean Foster <sbfoster@bu.edu>
2025-09-10 10:43:50 -0400

    Sync up mu-app with submodules

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,15 +1,116 @@
+BUILD_DATA_PRODUCTS=false
+BUILD_UNPACKERS=false
+BUILD_RECO=false
+
+# Help message
+show_help() {
+    echo "Usage: ./build_all.sh [OPTIONS]"
+    echo
+    echo "Options:"
+    echo "  -a, --all                   Build all components"
+    echo "  -d, --dataproducts          Build the data products"
+    echo "  -u, --unpackers             Build the unpackers"
+    echo "  -r, --reco                  Build the reco"
+    echo "  -j, --jobs <number>         Number of jobs"
+    echo "  -h, --help                  Display this help message"
+}
+
+JOBS_ARG="-j"
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -d|--dataproducts)
+            BUILD_DATA_PRODUCTS=true
+            shift
+            ;;
+        -u|--unpackers)
+            BUILD_UNPACKERS=true
+            shift
+            ;;
+        -r|--reco)
+            BUILD_RECO=true
+            shift
+            ;;
+        -a|--all)
+            BUILD_DATA_PRODUCTS=true
+            BUILD_UNPACKERS=true
+            BUILD_RECO=true
+            shift
+            ;;
+        -j|--jobs)
+            if [[ -n "$2" && "$2" != -* ]]; then
+                JOBS_ARG="-j $2"
+                shift 2
+            else
+                JOBS_ARG="-j"
+                shift
+            fi
+            ;;
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        *)
+            echo "[build_all.sh, ERROR] Unknown option: $1"
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+echo "Build all with job argument: ${JOBS_ARG}"
+
+# If not argument were provided, default to building all components
+if [ "$BUILD_DATA_PRODUCTS" = false ] && [ "$BUILD_UNPACKERS" = false ] && [ "$BUILD_RECO" = false ]; then
+    BUILD_DATA_PRODUCTS=true
+    BUILD_UNPACKERS=true
+    BUILD_RECO=true
+    echo "[build_all.sh] No specific packages specified, so building all packages."
+fi
+
+# Check some things. For instance, if you rebuild the data products, you should also rebuild everything!
+if [ "$BUILD_DATA_PRODUCTS" = true ]; then
+    if [ "$BUILD_UNPACKERS" = false ] || [ "$BUILD_RECO" = false ]; then
+        echo "[build_all.sh, WARNING] Force rebuilding unpackers and reco since data products are being rebuilt."
+        BUILD_UNPACKERS=true
+        BUILD_RECO=true
+    fi
+fi
+
+# Check if mu-unpackers is being rebuilt
+if [ "$BUILD_UNPACKERS" = true ]; then
+    if [ "$BUILD_RECO" = false ]; then
+        echo "[build_all.sh, WARNING] Force rebuilding reco since unpackers are being rebuilt."
+        BUILD_RECO=true
+    fi
+fi
+
+
 # Build data-products
-echo "[build_all.sh] Building data products..."
-./mu-data-products/scripts/build.sh -o
+if [ "$BUILD_DATA_PRODUCTS" = true ]; then
+    echo "[build_all.sh] Building data products..."
+    ./mu-data-products/scripts/build.sh -o ${JOBS_ARG}
+else
+    echo "[build_all.sh] Skipping data products build."
+fi
 
 # Build unpackers
-echo "[build_all.sh] Building unpackers..."
-./mu-unpackers/scripts/build.sh -o
+if [ "$BUILD_UNPACKERS" = true ]; then
+    echo "[build_all.sh] Building unpackers..."
+    ./mu-unpackers/scripts/build.sh -o ${JOBS_ARG}
+else
+    echo "[build_all.sh] Skipping unpackers build."
+fi
 
 # Build reco
-echo "[build_all.sh] Building reco..."
-./mu-reco/scripts/build.sh -o
+if [ "$BUILD_RECO" = true ]; then
+    echo "[build_all.sh] Building reco..."
+    ./mu-reco/scripts/build.sh -o ${JOBS_ARG}
+else
+    echo "[build_all.sh] Skipping reco build."
+fi
 
-# Build the app
+# Build the app always
 echo "[build_all.sh] Building the app..."
-./scripts/build.sh -o
+./scripts/build.sh -o ${JOBS_ARG}
--- src/main_wfd5.cpp
+++ src/main_wfd5.cpp
@@ -1,6 +1,6 @@
 /*
 
-Some description...
+This provides the main method for the nearline executable for processing WFD5 data.
 
 */
 
@@ -10,6 +10,7 @@ Some description...
 #include <fstream>
 #include <time.h>
 #include <filesystem>
+#include <chrono>
 
 // ROOT
 #include <TFile.h>
@@ -34,10 +35,8 @@ Some description...
 #include <reco/common/ConfigHolder.hh>
 // RecoStages
 #include <reco/common/RecoManager.hh>
-#include <reco/wfd5/JitterCorrector.hh>
 // Services
 #include <reco/common/ServiceManager.hh>
-#include <reco/wfd5/TemplateService.hh>
 
 #include <string>
 #include <sstream>
@@ -47,13 +46,15 @@ using unpackers::LoggerHolder;
 
 int main(int argc, char *argv[])
 {
+    // Start the clock!
+    auto start = std::chrono::high_resolution_clock::now();
     
     // -----------------------------------------------------------------------------------------------
     // Parse command line arguments
 
     // Parse the arguments
-    if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " configFile inputFileName verbosity" << std::endl;
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " configFile inputFileName outputFileName[optional]" << std::endl;
         return 1;
     }
     // config file name
@@ -62,8 +63,6 @@ int main(int argc, char *argv[])
     // input file name
     std::string input_file_name = argv[2];
 
-    int verbosity = std::atoi(argv[3]);
-
     // Check if input file exists
     if (!std::filesystem::exists(input_file_name)) {
         std::cerr << "Input file doesn't exist. Could not find " << input_file_name << std::endl;
@@ -71,8 +70,21 @@ int main(int argc, char *argv[])
     }
 
     // Check if config file exists
-    if (!std::filesystem::exists(config_file_name)) {
-        std::cerr << "Config file doesn't exist. Could not find " << config_file_name << std::endl;
+    std::string config_file_path = "";
+    if (config_file_name.find('/') != std::string::npos) {
+        // If not a base name, try using this path directly
+        config_file_path = config_file_name;
+    } else {
+        // Check if the file is in the current directory
+        if (std::filesystem::exists(config_file_name)) {
+            config_file_path = config_file_name;
+        } else {
+            // Otherwise, prepend the config directory from the environment variable
+            config_file_path = std::string(std::getenv("MU_RECO_PATH")) + "/config/" + config_file_name;
+        }
+    }
+    if (!std::filesystem::exists(config_file_path)) {
+        std::cerr << "Config file doesn't exist. Could not find " << config_file_path << std::endl;
         return 1;
     }
 
@@ -103,12 +115,15 @@ int main(int argc, char *argv[])
 
     // output file name
     std::string output_file_name;
-    output_file_name = input_file_name.substr(input_file_name.find_last_of("/\\") + 1);
-    output_file_name = output_file_name.substr(0, output_file_name.find_first_of('.')) + ".root";
+    if (argc > 3)
+    {
+        output_file_name = argv[3];
+    }
+    else{
+        output_file_name = input_file_name.substr(input_file_name.find_last_of("/\\") + 1);
+        output_file_name = output_file_name.substr(0, output_file_name.find_first_of('.')) + ".root";
+    }
     std::cout << "-> main: Output file: " << output_file_name << std::endl;
-
-    // Set verbosity for unpacker
-    LoggerHolder::getInstance().SetVerbosity(verbosity);
 
     // End of parsing command line arguments
     // -----------------------------------------------------------------------------------------------
@@ -119,7 +134,7 @@ int main(int argc, char *argv[])
 
     // Create configuration holder
     std::shared_ptr<reco::ConfigHolder> configHolder = std::make_shared<reco::ConfigHolder>();
-    configHolder->LoadFromFile(config_file_name);
+    configHolder->LoadFromFile(config_file_path);
     configHolder->SetRunSubrun(run, subrun);
 
     // Create the output manager
@@ -128,21 +143,42 @@ int main(int argc, char *argv[])
 
     // Create the midas event unpacker
     unpackers::EventUnpacker* eventUnpacker = new unpackers::WFD5EventUnpacker();
+    // Determine max number of midas events to process
+    int maxMidasEvents = -1;
+    if (configHolder->GetConfig().contains("Unpacker")) {
+        const auto& unpackerConfig = configHolder->GetConfig().at("Unpacker");
+        if (unpackerConfig.contains("max_midas_events") && unpackerConfig["max_midas_events"].is_number_integer()) {
+            maxMidasEvents = unpackerConfig.value("max_midas_events", -1);
+        }
+    } else {
+        throw std::runtime_error("Missing 'Unpacker' config section with 'max_midas_events'");
+    }
+    std::cout << "-> main: Max number of events to process: " << maxMidasEvents << std::endl;
 
+    // Set verbosity for unpacker
+    int unpackerVerbosity = 0; // Default verbosity
+    if (configHolder->GetConfig().contains("Unpacker")) {
+        const auto& unpackerConfig = configHolder->GetConfig().at("Unpacker");
+        if (unpackerConfig.contains("verbosity") && unpackerConfig["verbosity"].is_number_integer()) {
+            unpackerVerbosity = unpackerConfig.value("verbosity", 0);
+        }
+    } else {
+        std::cout << "-> main: Missing 'Unpacker' config section with 'verbosity'. Using default." << std::endl;
+    }
+    std::cout << "-> main: Setting unpacker verbosity to " << unpackerVerbosity << std::endl;
+    LoggerHolder::getInstance().SetVerbosity(unpackerVerbosity);
+    
     // Create the EventStore to hold all the data products during processing
     reco::EventStore eventStore;
+    eventStore.SetRunSubrun(run, subrun);
 
     // Create the service manager
-    reco::ServiceManager serviceManager;
-    serviceManager.Configure(configHolder);
+    std::shared_ptr<reco::ServiceManager> serviceManager = std::make_shared<reco::ServiceManager>();
+    serviceManager->Configure(configHolder, eventStore);
 
     // Create the reco manager
     reco::RecoManager recoManager;
-    recoManager.Configure(configHolder, serviceManager);
-
-    // Create some histograms
-    auto hist = std::make_shared<TH1D>("energy", "Energy Spectrum", 100, 0, 1000);
-    eventStore.putHistogram("energy", hist);
+    recoManager.Configure(configHolder, *serviceManager, eventStore);
 
     // Create the Midas reader
     TMReaderInterface *mReader = TMNewReader(input_file_name.c_str());
@@ -150,14 +186,26 @@ int main(int argc, char *argv[])
     // -----------------------------------------------------------------------------------------------
 
     // -----------------------------------------------------------------------------------------------
-    // Time to start the midas event loop
-    int nTotalMidasEvents = 0;
-    int nSkippedMidasEvents= 0;
+    // Prep for the midas event loop
+    int nProcessedMidasEvents = 0;
+    int nProcessedEvents = 0;
+    auto unpackerStart = std::chrono::high_resolution_clock::now();
+    auto unpackerStop = std::chrono::high_resolution_clock::now();
+    double unpackerDuration = 0;
+    auto writeStart = std::chrono::high_resolution_clock::now();
+    auto writeStop = std::chrono::high_resolution_clock::now();
+    double writeDuration = 0;
 
     // loop over the events
     while (true) {
+
+        if ((nProcessedMidasEvents >= maxMidasEvents) && (maxMidasEvents != -1)) {
+            // Reached the max number of events to process
+            break;
+        }
+
         TMEvent *thisEvent = TMReadEvent(mReader);
-        //if (!thisEvent || nTotalMidasEvents > 100 )
+        //if (!thisEvent || nProcessedEvents > 100 )
         if (!thisEvent)
         {
             // Reached end of the file. Clean up and break
@@ -202,30 +250,42 @@ int main(int argc, char *argv[])
 
         // only unpack events with id 1
         if (event_id == 1) {
-            nTotalMidasEvents++;
-            // std::cout << "Processing event " << nTotalMidasEvents << std::endl;
+            nProcessedMidasEvents++;
+            // std::cout << "Processing midas event " << nProcessedMidasEvents << std::endl;
             
             // Unpack the event; this is done in a loop in case there are multiple "trigger" events in the midas event
-            unpackers::unpackingStatus status = unpackers::unpackingStatus::Failure;
-            while ( (status = eventUnpacker->UnpackEvent(thisEvent)) == unpackers::unpackingStatus::SuccessMore) {
+            unpackers::unpackingStatus status = unpackers::unpackingStatus::SuccessMore;
+   
+            while (status == unpackers::unpackingStatus::SuccessMore) {
 
+                unpackerStart = std::chrono::high_resolution_clock::now();
+                status = eventUnpacker->UnpackEvent(thisEvent);
+                
+                if (status != unpackers::unpackingStatus::SuccessMore) {
+                    continue;
+                }
+                nProcessedEvents++;
+                
                 // Put the unpacked data into the event store
                 eventStore.clear();  // clear previous event's data
-                eventStore.put("unpacker","WFD5HeaderCollection", eventUnpacker->GetNextPtrCollection<dataProducts::WFD5Header>("WFD5HeaderCollection"));
-                eventStore.put("unpacker","WFD5ChannelHeaderCollection", eventUnpacker->GetNextPtrCollection<dataProducts::WFD5ChannelHeader>("WFD5ChannelHeaderCollection"));
-                eventStore.put("unpacker","WFD5WaveformHeaderCollection", eventUnpacker->GetNextPtrCollection<dataProducts::WFD5WaveformHeader>("WFD5WaveformHeaderCollection"));
-                eventStore.put("unpacker","WFD5WaveformCollection", eventUnpacker->GetNextPtrCollection<dataProducts::WFD5Waveform>("WFD5WaveformCollection"));
+                eventStore.put<dataProducts::WFD5Header>("unpacker","WFD5HeaderCollection", eventUnpacker->GetNextPtrCollection<dataProducts::WFD5Header>("WFD5HeaderCollection"));
+                eventStore.put<dataProducts::WFD5ChannelHeader>("unpacker","WFD5ChannelHeaderCollection", eventUnpacker->GetNextPtrCollection<dataProducts::WFD5ChannelHeader>("WFD5ChannelHeaderCollection"));
+                eventStore.put<dataProducts::WFD5WaveformHeader>("unpacker","WFD5WaveformHeaderCollection", eventUnpacker->GetNextPtrCollection<dataProducts::WFD5WaveformHeader>("WFD5WaveformHeaderCollection"));
+                eventStore.put<dataProducts::WFD5Waveform>("unpacker","WFD5WaveformCollection", eventUnpacker->GetNextPtrCollection<dataProducts::WFD5Waveform>("WFD5WaveformCollection"));
 
                 // Run reconstruction stages
                 try {
-                    recoManager.Run(eventStore, serviceManager);
+                    recoManager.Run(eventStore, *serviceManager);
                 } catch (const std::exception& e) {
                     std::cerr << "Error during reconstruction: " << e.what() << std::endl;
                     return 1;
                 }
 
                 // Fill the output tree with the event data
+                writeStart = std::chrono::high_resolution_clock::now();
                 outputManager->FillEvent(eventStore);
+                writeStop = std::chrono::high_resolution_clock::now();
+                writeDuration += std::chrono::duration<double>(writeStop - writeStart).count();
             }
 
             // Clean up the event now that we are done with it
@@ -236,15 +296,42 @@ int main(int argc, char *argv[])
 
     } // end loop over events
 
+    writeStart = std::chrono::high_resolution_clock::now();
     outputManager->WriteHistograms(eventStore);
+    outputManager->WriteSplines(eventStore);
+    writeStop = std::chrono::high_resolution_clock::now();
+    writeDuration += std::chrono::duration<double>(writeStop - writeStart).count();
 
-    // clean up
+    // Clean up
     delete eventUnpacker;
     delete outputManager;
     delete mReader;
 
-    std::cout << "-> main: Skipped " << nSkippedMidasEvents << "/" << nTotalMidasEvents << " midas events" << std::endl;
-
+    std::cout << "-> main: Processed " << nProcessedMidasEvents << " midas events" << std::endl;
+    std::cout << "-> main: Processed " << nProcessedEvents << " \"real\" events" << std::endl;
     std::cout << "-> main: All done!" << std::endl;
+
+    std::cout << "-> main: Unpacking took "
+        << std::setprecision(4) << unpackerDuration << "/" << nProcessedEvents
+        << " = "
+        << (unpackerDuration / nProcessedEvents) << " seconds/event"
+        << std::endl;
+
+    std::cout << "-> main: Filling tree / writing took "
+        << std::setprecision(4) << writeDuration << "/" << nProcessedEvents
+        << " = "
+        << (writeDuration / nProcessedEvents) << " seconds/event"
+        << std::endl;
+
+    // Print out end of job info for each service
+    std::cout << "-> main: Services summary:\n";
+    serviceManager->EndOfJobPrint();
+    
+    // Stop the clock!
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+
+    std::cout << std::setprecision(5) << "-> main: Total elapsed time: " << elapsed.count() << " seconds\n";
+
     return 0;
 }
